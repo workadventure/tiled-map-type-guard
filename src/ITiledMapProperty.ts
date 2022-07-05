d6f7ed9788b45f3cdb33862b2d0b3bdacfa776a0
David Négrier <d.negrier@thecodingmachine.com>
2022-07-05 09:12:09 +0200

    Adding stricter types for properties
    Using a discriminated union based on the "type" property to assert the type of the "value" property.

--- src/ITiledMapProperty.ts
+++ src/ITiledMapProperty.ts
@@ -1,10 +1,53 @@
 import { z } from 'zod';
 
-export const isTiledMapProperty = z.object({
+const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
+type Literal = z.infer<typeof literalSchema>;
+type Json = Literal | { [key: string]: Json } | Json[];
+const jsonSchema: z.ZodType<Json> = z.lazy(() =>
+  z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]),
+);
+
+const isTiledMapStringProperty = z.object({
   name: z.string(),
-  type: z.string(),
-  value: z.unknown().optional(),
+  type: z.union([z.literal('string'), z.literal('color'), z.literal('file')]),
+  value: z.string().optional(),
   propertytype: z.string().optional(),
 });
+
+const isTiledMapIntProperty = z.object({
+  name: z.string(),
+  type: z.union([z.literal('int'), z.literal('object')]),
+  value: z.number().int().optional(),
+  propertytype: z.string().optional(),
+});
+
+const isTiledMapFloatProperty = z.object({
+  name: z.string(),
+  type: z.literal('float'),
+  value: z.number().optional(),
+  propertytype: z.string().optional(),
+});
+
+const isTiledMapBoolProperty = z.object({
+  name: z.string(),
+  type: z.literal('bool'),
+  value: z.boolean().optional(),
+  propertytype: z.string().optional(),
+});
+
+const isTiledMapClassProperty = z.object({
+  name: z.string(),
+  type: z.literal('class'),
+  value: jsonSchema.optional(),
+  propertytype: z.string().optional(),
+});
+
+export const isTiledMapProperty = z.union([
+  isTiledMapStringProperty,
+  isTiledMapIntProperty,
+  isTiledMapFloatProperty,
+  isTiledMapBoolProperty,
+  isTiledMapClassProperty,
+]);
 
 export type ITiledMapProperty = z.infer<typeof isTiledMapProperty>;
